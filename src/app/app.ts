f45fd1330bc31fea64e3d4f6a06cc30f25fbde1d
siddzoha <siddzohnexta@sheridancollege.ca>
2025-09-19 22:04:42 -0400

    Commit 1: init commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,33 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor } from '@angular/common';
+import { HeaderComponent } from './header/header';
+import { FooterComponent } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css'],
+  standalone: true,
+  imports: [NgFor, HeaderComponent, FooterComponent]
 })
 export class App {
-  protected readonly title = signal('six-cards-demo');
+  cards: string[] = [
+    'card_2.png', 'card_3.png','card_4.png','card_5.png','card_6.png',
+  ];
+
+  sortCards() {
+    this.cards.sort();
+  }
+
+  reverseCards() {
+    this.cards.reverse();
+  }
+
+  shuffleCards() {
+    for ( let i = this.cards.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [this.cards[i], this.cards[j]] = [this.cards[i], this.cards[j]];
+    }
+  }
 }
